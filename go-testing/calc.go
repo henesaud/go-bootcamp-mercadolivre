a4b86f4de6907cab3826fb89370da4baf053449c
henesaud <hene.saud@mercadolivre.com>
2024-04-24 14:19:50 -0300

    feat: devide function and error

--- go-testing/calc.go
+++ go-testing/calc.go
@@ -1,9 +1,14 @@
 package cal
 
+import "errors"
+
 func Sub(num1, num2 int) int {
 	return num1 - num2
 }
 
-func Divide(num, den int) int {
-	return num / den
+func Divide(num, den int) (int, error) {
+	if den == 0 {
+		return 0, errors.New("denominator cannot be zero")
+	}
+	return num / den, nil
 }
--- go-testing/calc_test.go
+++ go-testing/calc_test.go
@@ -10,3 +10,12 @@ func TestSub(t *testing.T) {
 	result := Sub(10, 5)
 	assert.Equal(t, 5, result, "should be equal")
 }
+
+func TestDivide(t *testing.T) {
+	result, err := Divide(10, 5)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, result)
+
+	_, err = Divide(10, 0)
+	assert.NotNil(t, err)
+}
